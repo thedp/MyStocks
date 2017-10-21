71968e6abda3deebe7cb9857bb115a2e19293fdc
eldare <eldare@gmail.com>
2017-10-21 13:54:31 +0300

    StockCell init

--- MyStocks/MainView.swift
+++ MyStocks/MainView.swift
@@ -12,16 +12,25 @@ class MainView: UITableViewController {
 
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 1  // TODO:
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StockCell", for: indexPath)
 
         return cell
     }
+}
+
+class StockCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var symbolLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var trendLabel: UILabel!
+    @IBOutlet weak var graphView: UIView!
 }
